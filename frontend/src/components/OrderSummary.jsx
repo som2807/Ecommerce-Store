1ef34fe750f9c7eb2287dcc2229d0392e19f763c
SOUMYADIP MAJI <smaji2807@gmail.com>
2025-09-10 15:40:55 +0530

    Update OrderSummary.jsx

--- frontend/src/components/OrderSummary.jsx
+++ frontend/src/components/OrderSummary.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import axios from "../lib/axios";
 
 const stripePromise = loadStripe(
-	"pk_test_51KZYccCoOZF2UhtOwdXQl3vcizup20zqKqT9hVUIsVzsdBrhqbUI2fE0ZdEVLdZfeHjeyFXtqaNsyCJCmZWnjNZa00PzMAjlcL"
+	"pk_test_51S5YGmJwrT9thxIRUO2sHxnBWGUn8Ba66H7qS1RvIYTs7ZGojhZiU83bp8gJf0ko1mVRbhuiW2G6G8VEuAcwWSPK007ELRwBQV"
 );
 
 const OrderSummary = () => {
